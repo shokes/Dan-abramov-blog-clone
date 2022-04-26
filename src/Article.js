bfc7d6dc76f78707b9e2059eafd0eb30331949a4
shokes <oyatioshoke@yahoo.com>
2022-04-26 08:02:07 +0100

    additon of an animation

--- src/Article.js
+++ src/Article.js
@@ -1,22 +1,25 @@
 import moment from 'moment';
+import { Zoom } from 'react-awesome-reveal';
 
 const Article = function ({ title, date, length, snippet }) {
   const formattedDate = moment(date).format('MMMM Do, YYYY');
   return (
-    <main className='article'>
-      <div>
-        {/* <h2>{title}</h2> */}
-        <a className='article-heading' href='#'>
-          {title}
-        </a>
-        <div className='time-flex'>
-          <p>{formattedDate}.</p>
-          <p className='emoji'>{length} min read</p>
-        </div>
+    <Zoom triggerOnce>
+      <main className='article'>
+        <div>
+          {/* <h2>{title}</h2> */}
+          <a className='article-heading' href='#'>
+            {title}
+          </a>
+          <div className='time-flex'>
+            <p>{formattedDate}.</p>
+            <p className='emoji'>{length} min read</p>
+          </div>
 
-        <p className='para'>{snippet}</p>
-      </div>
-    </main>
+          <p className='para'>{snippet}</p>
+        </div>
+      </main>
+    </Zoom>
   );
 };
 
